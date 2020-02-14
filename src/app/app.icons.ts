2a6f83e427a37018a07dca6e77e92d7fa2406de6
Daniel Schiramm <dss.schiramm@gmail.com>
2020-02-14 07:31:19 -0300

    fontAwesome icons module

---
+++ src/app/app.icons.ts
@@ -0,0 +1,7 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faStar as farStar } from "@fortawesome/free-regular-svg-icons";
+import { faStar as fasStar } from "@fortawesome/free-solid-svg-icons";
+
+const Icons = [faGithub, farStar, fasStar];
+
+export { Icons };
